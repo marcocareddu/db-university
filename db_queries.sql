8a833666acc91ecaf15d28cdae370b4c52af3388
marcocareddu <saldumaccu@tuta.io>
2023-07-28 15:13:42 +0200

    SQL - First answer

--- db_queries.sql
+++ db_queries.sql
@@ -2,8 +2,11 @@
 -- -------------------------------------------------------------
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
+SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
+
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
