cb1e1013987fd5222bd2d4820c7bfa462325224d
marcocareddu <saldumaccu@tuta.io>
2023-07-28 15:47:13 +0200

    SQL - Eighth answer

--- db_queries.sql
+++ db_queries.sql
@@ -29,6 +29,7 @@ SELECT COUNT(*) AS `Numero dipartimenti` FROM `departments`;
 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 
 
