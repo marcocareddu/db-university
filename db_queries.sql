efeee203420e1b0f1b8ee2e5be872ce346ee37e5
marcocareddu <saldumaccu@tuta.io>
2023-07-28 15:23:23 +0200

    SQL - Fourth answer

--- db_queries.sql
+++ db_queries.sql
@@ -16,6 +16,7 @@ SELECT * FROM `students` WHERE (2023 - YEAR(`date_of_birth`)) > 30;
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * FROM `courses` WHERE `period` LIKE 'I %' AND `year` = 1;
 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
