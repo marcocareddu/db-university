ce8796dfbda78f729913826b8ccf2b110723012d
marcocareddu <saldumaccu@tuta.io>
2023-07-28 15:35:31 +0200

    SQL - Fifth answer

--- db_queries.sql
+++ db_queries.sql
@@ -20,6 +20,7 @@ SELECT * FROM `courses` WHERE `period` LIKE 'I %' AND `year` = 1;
 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE DATE(`date`) = '2020-06-20' AND TIME(`hour`) > '14:00';
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
